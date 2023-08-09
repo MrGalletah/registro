afcdaaafc06e3c83b3f8e5da01aad468e8be4568
Rodrigo Martinez <ropecos94@gmail.com>
2023-08-09 14:00:12 -0300

    Update validacion.js

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,23 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+document.addEventListener('DOMContentLoaded', function(){
+    const registroBoton = document.getElementById('regBtn');
+    
+    registroBoton.addEventListener('click', function(evento) {
+        const nombre = document.getElementById('nombre').value;
+        const apellido = document.getElementById('apellido').value;
+        const email = document.getElementById('email').value;
+        const constrasena1 = document.getElementById('password1').value;
+        const contrasena2 = document.getElementById('password2').value;
+        const terminos = document.getElementById('terminos').checked;
+        
+        if (nombre === '' || apellido === '' || email === '' || constrasena1 === '' || contrasena2 === '' || !terminos) {
+            evento.preventDefault();
+            showAlertError();
+        }else{
+            showAlertSuccess();
+        }
+    });
+});
